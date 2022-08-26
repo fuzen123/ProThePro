4c78d379f78b2eccf456ca2e7f6b5d7cc0c3897f
fuzen123 <kobeivan@inet.hr>
2022-08-26 22:02:20 +0200

    Revert "Update ShootWave.cs"
    This reverts commit 49336eb25d1bb6c6977f981c4db83fc3e4ee5b86.

--- Assets/_gameCore/Scripts/ShootWave.cs
+++ Assets/_gameCore/Scripts/ShootWave.cs
@@ -6,7 +6,6 @@ public class ShootWave : MonoBehaviour
 
     public bool ShootSwitch { set { isReadyToFireOff = value; } }
     private bool isReadyToFireOff = true;
-    public float SomeTestV = 1f;
 
     private void ShootRing()
     {
@@ -15,7 +14,6 @@ public class ShootWave : MonoBehaviour
     void Start()
     {
         WaveAnim.Init(this);
-        SomeTestV *= 10f;
     }
 
     void Update()
