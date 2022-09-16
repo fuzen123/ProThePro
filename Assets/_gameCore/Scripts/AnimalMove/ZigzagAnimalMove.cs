23c636a28dd8b84b87696413d981e34fbcb53d78
fuzen123 <kobeivan@inet.hr>
2022-09-16 12:28:03 +0200

    add animal
    new animal and movement type

--- Assets/_gameCore/Scripts/AnimalMove/ZigzagAnimalMove.cs
+++ Assets/_gameCore/Scripts/AnimalMove/ZigzagAnimalMove.cs
@@ -4,8 +4,21 @@ using UnityEngine;
 
 public class ZigzagAnimalMove : AnimalMoving
 {
+    public float frequency = 10.0f; // Speed of sine movement
+    public float magnitude = 1.0f; //  Size of sine movement, its the amplitude of the side curve
+
     public override void Update()
     {
+        SetZigZagDirection();
         base.Update();
+    }
+
+    private void SetZigZagDirection()
+    {
+        direction = _player.position - transform.position;
+        float angle = Mathf.Sin(Time.time * frequency) * magnitude;
+        direction = Quaternion.Euler(0f, angle, 0f) * direction;
+
+        transform.rotation = Quaternion.LookRotation(direction);
     }
 }
